d60cb97d3baab8f78a39b511c66ed1bc4a715e97
Dylan Manuel <dylan.manuel@att.net>
2026-06-20 19:01:18 -0500

    chore: add mimalloc to app

--- crates/app/src-tauri/src/main.rs
+++ crates/app/src-tauri/src/main.rs
@@ -1,5 +1,9 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+use mimalloc::MiMalloc;
+
+#[global_allocator]
+static GLOBAL: MiMalloc = MiMalloc;
 
 fn main() {
     codablellm_lib::run()
